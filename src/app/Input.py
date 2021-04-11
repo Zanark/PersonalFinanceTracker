0aac70aff56c9f3c460abe499cfa068da8565dd6
Zanark <zanarkdev@gmail.com>
2021-04-11 15:41:44 +0530

    Added a wireframe for the application

---
+++ src/app/Input.py
@@ -0,0 +1,46 @@
+# Welcome Message
+
+# Ask for Fiscal Year
+
+# Ask for Month
+
+# Ask for budget for the month
+
+# Ask for Bank Name
+
+# Ask for date of expense
+
+# Ask for Expense Catagory
+
+#Show following expense categories to chose from
+# 1. Salary         Green
+# 2. Transfer       Violet
+# 3. Investment     Blue
+# 4. Utilities      Grey
+# 5. Hobby          Orange
+# 6. Reimbursable   Yellow
+# 7. Others         Normal
+
+# Under reimburable category, the first entry will have a red bar nad teh second entry confirming the reimbursement will have a green bar
+
+# If 1 is chosen, then ask for the following:
+# Basic Salary
+# HRA
+# Special Allowance
+# Ask if anything added?
+# Deductions
+
+# All this goes under the details section
+
+# If amount added is +ve then add to Credit column else add to Debit column
+
+# After every entry update the closing balance variable and write the value after every step in the last column
+
+# At the end mention the total amount expended under each category with a section for final balance
+
+# Repeat from 'Ask for Bank Name'
+
+# Check if the monthly budget was followed using the Expenses in Utilities, Hobby and Others and plot a bar graph for the difference.
+
+# Repeat from 'Ask for Month'
+
